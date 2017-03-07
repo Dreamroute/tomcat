0784c68aa5f00e08e3fec7ade6a889edf83c24f5
342252328@qq.com <342252328@qq.com>
2017-03-07 22:34:57 +0800

    a

---
+++ src/main/java/com/mook/tomcat/second/PrimitiveServlet.java
@@ -0,0 +1,42 @@
+package com.mook.tomcat.second;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class PrimitiveServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.err.println("init.");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.err.println("from service.");
+        PrintWriter out = response.getWriter();
+        out.println("Hello.");
+        out.println("Leo.");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.err.println("destroy.");
+    }
+
+}
